3969b151f612b0e2c102bf6408f3c8c0c9e4851d
Danna <163548679+DannVal9@users.noreply.github.com>
2024-04-19 08:59:50 -0500

    Update login.php

--- CAPUCHINO/login.php
+++ CAPUCHINO/login.php
@@ -4,7 +4,7 @@
 	<meta charset="UFT-8">
 	<meta charset="X-UA-Compatible" content="IE-edge">
 	<meta name="viewport" content="width-device-width, initial-scale=1.0">
-	<title>DATABASE REGISTRO</title>
+	<title>DATABASE LOGIN</title>
 	<link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 
